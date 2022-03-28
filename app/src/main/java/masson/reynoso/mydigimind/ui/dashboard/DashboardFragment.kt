2461fe6b2784f8fad22d29bdb6f28705b016c831
JorgeMasson <j_masson_reynoso@hotmail.com>
2022-03-27 19:38:43 -0700

    Clases HomeFragment y DashboardFragment editadas

--- app/src/main/java/masson/reynoso/mydigimind/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/masson/reynoso/mydigimind/ui/dashboard/DashboardFragment.kt
@@ -6,11 +6,30 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import masson.reynoso.mydigimind.R
 import masson.reynoso.mydigimind.databinding.FragmentDashboardBinding
+import java.util.zip.Inflater
 
 class DashboardFragment : Fragment() {
 
+    private lateinit var dashboardViewModel: DashboardViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        dashboardViewModel = ViewModelProvider(this).get(dashboardViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_dashboard, container, false)
+        dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
+
+        })
+        return root
+    }
+
+    /*
     private var _binding: FragmentDashboardBinding? = null
 
     // This property is only valid between onCreateView and
@@ -39,4 +58,5 @@ class DashboardFragment : Fragment() {
         super.onDestroyView()
         _binding = null
     }
+    */
 }
--- app/src/main/java/masson/reynoso/mydigimind/ui/home/HomeFragment.kt
+++ app/src/main/java/masson/reynoso/mydigimind/ui/home/HomeFragment.kt
@@ -6,11 +6,30 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import masson.reynoso.mydigimind.R
 import masson.reynoso.mydigimind.databinding.FragmentHomeBinding
+import java.util.zip.Inflater
 
 class HomeFragment : Fragment() {
 
+    private lateinit var homeViewModel: HomeViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        homeViewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_home, container, false)
+        homeViewModel.text.observe(viewLifecycleOwner, Observer {
+
+        })
+        return root
+    }
+
+    /*
     private var _binding: FragmentHomeBinding? = null
 
     // This property is only valid between onCreateView and
@@ -39,4 +58,6 @@ class HomeFragment : Fragment() {
         super.onDestroyView()
         _binding = null
     }
+    */
+
 }
