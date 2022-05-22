f09977829f4c77c4ae45fee41213a1c100df4ef2
JorgeMasson <j_masson_reynoso@hotmail.com>
2022-05-22 03:20:13 -0700

    login terminado

--- app/src/main/java/masson/reynoso/mydigimind/ContrasenaActivity.kt
+++ app/src/main/java/masson/reynoso/mydigimind/ContrasenaActivity.kt
@@ -1,9 +1,11 @@
 package masson.reynoso.mydigimind
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
@@ -20,9 +22,16 @@ class ContrasenaActivity : AppCompatActivity() {
         auth = Firebase.auth
 
         val btn_restablecer: Button = findViewById(R.id.btn_restablecer)
+        val tv_inicio_sesion: TextView = findViewById(R.id.tv_iniciarSesion)
 
         btn_restablecer.setOnClickListener {
             validaCorreo()
+        }
+
+        tv_inicio_sesion.setOnClickListener {
+            val intent: Intent = Intent(this, LoginActivity::class.java)
+
+            startActivity(intent)
         }
 
     }
