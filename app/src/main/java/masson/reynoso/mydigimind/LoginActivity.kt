ccc1fa6028f1a5dc314aa362025aa11bc13a617a
JorgeMasson <j_masson_reynoso@hotmail.com>
2022-05-22 02:15:37 -0700

    logica del comportamiento de pantallas

--- app/src/main/java/masson/reynoso/mydigimind/LoginActivity.kt
+++ app/src/main/java/masson/reynoso/mydigimind/LoginActivity.kt
@@ -1,11 +1,53 @@
 package masson.reynoso.mydigimind
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val btn_Registrar: Button = findViewById(R.id.btn_registrarse)
+        val contrasena: TextView = findViewById(R.id.tv_olvidasteContra)
+        val btn_IniciarSesion: Button = findViewById(R.id.btn_iniciarSesion)
+
+        btn_Registrar.setOnClickListener {
+            val intent: Intent = Intent(this,RegistroActivity::class.java)
+            startActivity(intent)
+        }
+
+        contrasena.setOnClickListener {
+            val intent: Intent = Intent(this,ContrasenaActivity::class.java)
+            startActivity(intent)
+        }
+
+        btn_IniciarSesion.setOnClickListener {
+            validaSesion()
+        }
+
     }
+
+    private fun validaSesion() {
+
+        val et_correo: EditText = findViewById(R.id.correoElectronico)
+        val et_contra: EditText = findViewById(R.id.contraseña)
+
+        var correo: String = et_correo.text.toString()
+        var contra: String = et_contra.text.toString()
+
+        if(!correo.isNullOrBlank() && contra.isNullOrBlank()){
+
+        }else{
+            Toast.makeText(this,"Ingresar datos",Toast.LENGTH_SHORT).show()
+        }
+
+
+    }
+
 }
